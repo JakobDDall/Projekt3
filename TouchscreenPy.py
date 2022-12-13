a84e15a7b52f28629122f4ecd6681d7f15d6fa3a
FrederikKongstedMadsen <202105416@post.au.dk>
2022-12-13 16:33:55 +0100

    helt nyt

--- TouchscreenPy.py
+++ TouchscreenPy.py
@@ -5,16 +5,17 @@ import spidev
    
 class TouchscreenPy:
     
-    def __init__(self, advancedmode, simplemode):
-        self.advancedmode = advancedmode
-        self.simplemode = simplemode
+    def __init__(self, advancedMode_, simpleMode_):
+        #Initiere de enkelte modes:
+        self.advancedMode_ = advancedMode_
+        self.advancedMode_ = simpleMode_
     
         
     def toggleSimple(self):
-        self.simplemode = True
+        #Vælger simple mode og disabler advanced knappen:
+        self.simpleMode_ = True
         self.simpleButton.enabled = False
         self.advancedButton.enabled = False
-        print("Simple funktion er valgt")
         
         #Skriv til filen at simple er valgt:
         text = "Simple\n"
@@ -22,10 +23,10 @@ class TouchscreenPy:
         self.writeToFile(lineNumber,text)
         
     def toggleAdvanced(self):
-        self.advancedmode = True
+        #Vælger advanced mode og disabler simple knappen:
+        self.advancedMode_ = True
         self.advancedButton.enabled = False
         self.simpleButton.enabled = False
-        #self.valgtMode.value = "Avanceret funktion er valgt"
         
         #Skriv til filen at advanced er valgt:
         text = "Advanced\n"
@@ -33,8 +34,8 @@ class TouchscreenPy:
         self.writeToFile(lineNumber,text)
            
     def stopButton(self):
-        self.advancedmode = False
-        self.simplemode = False
+        self.simpleMode_ = False
+        self.simpleMode_ = False
         self.advancedButton.enabled = True
         self.simpleButton.enabled = True
         self.valgtMode.value = "Ingen funktion er valgt"
@@ -54,7 +55,7 @@ class TouchscreenPy:
         data = read_f.readlines()
         read_f.close()
         
-        #Write:
+        #Skriver userinput til filen:
         write_f = open("Screen.txt",'w')
         data[lineNumber] = text
         write_f.writelines(data)
@@ -62,7 +63,7 @@ class TouchscreenPy:
         print(data)
         
     def readData(self, lineNumber):
-        #Read:
+        #Læser bestemte line fra filen.
         read_f = open("Screen.txt",'r')
         data = read_f.readlines()
         lineType = data[lineNumber]
@@ -70,9 +71,9 @@ class TouchscreenPy:
         read_f.close()    
         return lineType
             
-    #Main:
-    def main(self):
-        if self.simplemode == True or self.advancedmode == True:
+    #DisplayData (Er kun gyldigt når simpleMode_ eller advancedMode er true:
+    def displayData(self):
+        if self.simpleMode_ == True or self.advancedMode_ == True:
         #Kører callback:
            nextMove = self.readData(3)
            self.move.value = nextMove
@@ -86,7 +87,7 @@ class TouchscreenPy:
            self.valgtMode.value = self.readData(0)
         
             
-            
+    #Herunder er selve interfacet:   
     def GUI(self):
         ###### Visualisering af knapper og text: ######################
         self.app = App('Hovedmenu for LineBot', height = 480, width = 800)
@@ -112,7 +113,7 @@ class TouchscreenPy:
         
         Text(self.app, text="Nuværende linjetype:", align ="top")
         self.linje = Text(self.app, text="Ingen linjetype")
-        self.linje.repeat(100, self.main)  # Laver callback hvert 100ms
+        self.linje.repeat(100, self.displayData)  # Laver callback hvert 100ms
         
         Text(self.app, text="Næste move:", align ="top")
         self.move = Text(self.app, text="Ingen")
@@ -124,7 +125,7 @@ class TouchscreenPy:
         self.app.display()
             
         
-            
+#Opret klassen, samt konstant opdater ved kald af GUI.
 H_menu = TouchscreenPy(False,False)
 H_menu.GUI()
 
